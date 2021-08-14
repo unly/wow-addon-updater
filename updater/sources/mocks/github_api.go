e2da0749cda88416a2c6b9dc5021ab72699f5059
Leon Graser <extern.leon.graser@volkswagen.de>
2021-08-14 23:24:29 +0200

    update the version of dependencies

--- updater/sources/mocks/github_api.go
+++ updater/sources/mocks/github_api.go
@@ -5,7 +5,7 @@ package mocks
 import (
 	context "context"
 
-	github "github.com/google/go-github/v32/github"
+	github "github.com/google/go-github/v38/github"
 	mock "github.com/stretchr/testify/mock"
 )
 
