a37bda7b701ca5bd9db9c39dd764efc0fffcf326
unly <leongrch@gmail.com>
2021-10-30 18:34:59 +0200

    simplify config tests (#33)

--- config/config_test.go
+++ config/config_test.go
@@ -9,51 +9,36 @@ import (
 	"github.com/unly/wow-addon-updater/util/tests/helpers"
 )
 
-func Test_ReadConfig(t *testing.T) {
-	type readConfigTest struct {
-		file          string
-		want          Config
-		errorExpected bool
-		teardown      helpers.TearDown
-	}
+func TestReadConfig(t *testing.T) {
+	t.Run("current dir", func(t *testing.T) {
+		_, err := ReadConfig(".")
 
-	tests := []func() *readConfigTest{
-		func() *readConfigTest {
-			return &readConfigTest{
-				file:          ".",
-				want:          Config{},
-				errorExpected: true,
-				teardown:      helpers.NoopTeardown(),
-			}
-		},
-		func() *readConfigTest {
-			return &readConfigTest{
-				file:          "",
-				want:          Config{},
-				errorExpected: true,
-				teardown:      helpers.NoopTeardown(),
-			}
-		},
-		func() *readConfigTest {
-			file := helpers.TempFile(t, "", []byte("hello world"))
-			return &readConfigTest{
-				file:          file,
-				want:          Config{},
-				errorExpected: true,
-				teardown:      helpers.DeleteFile(t, file),
-			}
-		},
-		func() *readConfigTest {
-			file := helpers.TempFile(t, "", []byte{})
-			return &readConfigTest{
-				file:          file,
-				want:          Config{},
-				errorExpected: false,
-				teardown:      helpers.DeleteFile(t, file),
-			}
-		},
-		func() *readConfigTest {
-			content := []byte(`
+		assert.Error(t, err)
+	})
+	t.Run("empty string", func(t *testing.T) {
+		_, err := ReadConfig("")
+
+		assert.Error(t, err)
+	})
+	t.Run("invalid file content", func(t *testing.T) {
+		file := helpers.TempFile(t, "", []byte("hello world"))
+		defer helpers.DeleteFile(t, file)
+
+		_, err := ReadConfig(file)
+
+		assert.Error(t, err)
+	})
+	t.Run("empty file", func(t *testing.T) {
+		file := helpers.TempFile(t, "", []byte{})
+		defer helpers.DeleteFile(t, file)
+
+		cfg, err := ReadConfig(file)
+
+		assert.NoError(t, err)
+		assert.Equal(t, Config{}, cfg)
+	})
+	t.Run("sample config", func(t *testing.T) {
+		content := []byte(`
 classic:
   path: path/to/classic
   addons:
@@ -64,87 +49,54 @@ retail:
   addons:
     - addon3
     - addon4`)
-			file := helpers.TempFile(t, "", content)
-			return &readConfigTest{
-				file: file,
-				want: Config{
-					Classic: WowConfig{
-						Path: "path/to/classic",
-						AddOns: []string{
-							"addon1",
-							"addon2",
-						},
-					},
-					Retail: WowConfig{
-						Path: "path/to/retail",
-						AddOns: []string{
-							"addon3",
-							"addon4",
-						},
-					},
+		file := helpers.TempFile(t, "", content)
+		defer helpers.DeleteFile(t, file)
+
+		cfg, err := ReadConfig(file)
+
+		assert.NoError(t, err)
+		want := Config{
+			Classic: WowConfig{
+				Path: "path/to/classic",
+				AddOns: []string{
+					"addon1",
+					"addon2",
 				},
-				errorExpected: false,
-				teardown:      helpers.DeleteFile(t, file),
-			}
-		},
-	}
-
-	for _, fn := range tests {
-		tt := fn()
-
-		cfg, err := ReadConfig(tt.file)
-
-		if tt.errorExpected {
-			assert.Error(t, err)
-		} else {
-			assert.NoError(t, err)
-			assert.Equal(t, tt.want, cfg)
+			},
+			Retail: WowConfig{
+				Path: "path/to/retail",
+				AddOns: []string{
+					"addon3",
+					"addon4",
+				},
+			},
 		}
-
-		tt.teardown()
-	}
+		assert.Equal(t, want, cfg)
+	})
 }
 
-func Test_CreateDefaultConfig(t *testing.T) {
-	type createDefaultConfigTest struct {
-		file          string
-		errorExpected bool
-		teardown      helpers.TearDown
-	}
-
+func TestCreateDefaultConfig(t *testing.T) {
 	dir := helpers.TempDir(t)
 	defer helpers.DeleteDir(t, dir)()
 
-	tests := []func() *createDefaultConfigTest{
-		func() *createDefaultConfigTest {
-			file := filepath.Join(dir, "file1")
-			return &createDefaultConfigTest{
-				file:          file,
-				errorExpected: false,
-				teardown:      helpers.DeleteDir(t, file),
-			}
-		},
-		func() *createDefaultConfigTest {
-			file := helpers.TempFile(t, dir, []byte{})
-			assert.NoError(t, os.Chmod(file, os.FileMode(0400)))
-			return &createDefaultConfigTest{
-				file:          file,
-				errorExpected: true,
-				teardown:      helpers.DeleteDir(t, file),
-			}
-		},
-	}
+	t.Run("create default config", func(t *testing.T) {
+		file := filepath.Join(dir, "file1")
 
-	for _, fn := range tests {
-		tt := fn()
-		err := CreateDefaultConfig(tt.file)
+		err := CreateDefaultConfig(file)
 
-		if tt.errorExpected {
-			assert.Error(t, err)
-		} else {
-			assert.NoError(t, err)
+		assert.NoError(t, err)
+		helpers.DeleteDir(t, file)
+	})
+	t.Run("existing read only file", func(t *testing.T) {
+		file := helpers.TempFile(t, dir, []byte{})
+		err := os.Chmod(file, os.FileMode(0400))
+		if err != nil {
+			assert.FailNow(t, "failed to chmod 04400")
 		}
 
-		tt.teardown()
-	}
+		err = CreateDefaultConfig(file)
+
+		assert.Error(t, err)
+		helpers.DeleteDir(t, file)
+	})
 }
